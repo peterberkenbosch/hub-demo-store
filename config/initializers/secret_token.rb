97123292a8158183084cb4ba1ac27b2bc87ebd88
Peter Berkenbosch <peter@pero-ict.nl>
2014-04-02 22:00:51 +0200

    created rails 4.0.4 skeleton with postgresql support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HubDemoStore::Application.config.secret_key_base = 'd2c557f42c391a28dd08494a1050b6a7e11fffdb63a43c93a9ad14e78752988433ea87324c21c1d6ee62b109e2d823e462280b4fff0ea918285feb40fd7cdda4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HubDemoStore::Application.config.session_store :cookie_store, key: '_hub-demo-store_session'
